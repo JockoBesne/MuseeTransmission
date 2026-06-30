b5afd810b88318b889f313c9029789f8a634c9fb
rkNyAvo <ramiandrisoa.nyavo@gmail.com>
2026-06-30 17:13:36 +0200

    fix zoom de la carte

--- src/components/map/InteractiveMap.tsx
+++ src/components/map/InteractiveMap.tsx
@@ -10,7 +10,7 @@ const villes = villesData as FeatureCollection<Point>;
 
 function pointToLayer(_feature: Feature<Point>, latlng: L.LatLng) {
   return L.circleMarker(latlng, {
-    radius: 3,
+    radius: 6,
     fillColor: '#ff8200',
     color: '#ffffff',
     weight: 2,
@@ -20,7 +20,7 @@ function pointToLayer(_feature: Feature<Point>, latlng: L.LatLng) {
 
 function onEachCityFeature(feature: Feature<Point>, layer: L.Layer) {
   const props = feature.properties as { nom: string; texte: string };
-  layer.bindPopup(`<strong>${props.nom}</strong><br/>${props.texte}`);
+  layer.bindPopup(`<strong>${props.nom}</strong><br/>`);
 }
 
 export default function InteractiveMap() {
@@ -28,6 +28,10 @@ export default function InteractiveMap() {
     <MapContainer
       style={{ height: '100%', width: '100%' , background: '#0f70b7'}}
       attributionControl={false}
+      center={[46.5, 2.3]}
+      minZoom={6}
+      maxZoom={6}
+      zoom={6}
       bounds={[[51.5, -5.5], [41.2, 9.8]]}
       zoomControl={false}
       dragging={false}
