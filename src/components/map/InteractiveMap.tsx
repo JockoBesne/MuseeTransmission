cbfbbd3cc834e8722a15da9e60aacaf14fd754b0
cadji <chene.adji@etudiant.univ-rennes.fr>
2026-07-01 16:19:06 +0200

    noms des villes sans bulles + boit de dial en largeur

--- src/components/map/InteractiveMap.tsx
+++ src/components/map/InteractiveMap.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { MapContainer, GeoJSON } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import './InteractiveMap.css';
 import type { Feature, FeatureCollection, MultiPolygon, Point } from 'geojson';
 import franceContourRaw from '../../data/france-contour.json';
 import villesData from '../../data/villes.json';
