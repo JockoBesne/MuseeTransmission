77150a4d8d65ef5ba6b3909dc99356e1ee3f19b8
cadji <chene.adji@etudiant.univ-rennes.fr>
2026-07-22 13:08:29 +0200

    maj memo

--- src/components/Memorial/Memorial.tsx
+++ src/components/Memorial/Memorial.tsx
@@ -364,7 +364,6 @@ export default function Memorial() {
           ))}
         </div>
         <p className="war-hint">‹&nbsp;&nbsp;Glissez pour changer de période&nbsp;&nbsp;›</p>
-        <p className="memorial-subtitle">{WAR_LABELS[war]}</p>
       </div>
 
       <div className="memorial-search">
