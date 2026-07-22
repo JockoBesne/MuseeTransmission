16f95e1f5db5b00c942d46ad3177b3554fc8372d
cadji <chene.adji@etudiant.univ-rennes.fr>
2026-07-22 16:59:57 +0200

    retrait selection tactile

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import './fonts.css'
 import './index.css'
 import App from './App.tsx'
 
+// Borne tactile : bloque le menu contextuel (appui long / clic droit) qui
+// permettrait de sortir du contenu en mode kiosque.
+document.addEventListener('contextmenu', (e) => e.preventDefault())
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
