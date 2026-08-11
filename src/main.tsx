089035de7ac59a0a908801d61d22a95e980c0601
cadji <chene.adji@etudiant.univ-rennes.fr>
2026-08-11 14:44:37 +0200

    multitouch (correction tactile)

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,15 @@ import './fonts.css'
 import './index.css'
 import App from './App.tsx'
 import { installWatchdog } from './utils/watchdog'
+import { installMultiTouchTap } from './utils/multiTouchTap'
 
 // Borne tactile : bloque le menu contextuel (appui long / clic droit) qui
 // permettrait de sortir du contenu en mode kiosque.
 document.addEventListener('contextmenu', (e) => e.preventDefault())
+
+// Rend les boutons cliquables quand un second doigt est déjà posé ailleurs
+// sur la dalle (voir le commentaire du module).
+installMultiTouchTap()
 
 // Avant le rendu : une erreur au tout premier montage doit déjà être rattrapée.
 installWatchdog()
